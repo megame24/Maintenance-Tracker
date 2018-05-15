032904711b81ebf94700d89a2e9e134e90f7a857
Innocent Ngene <ngeneinnocent@gmail.com>
2018-05-15 11:41:42 +0100

    [Feature #157589678] set up endpoint for creating requests

--- server/controllers/requestsController.js
+++ server/controllers/requestsController.js
@@ -2,7 +2,7 @@ import requests from '../db/requests';
 
 const requestsController = {
   getRequests(req, res) {
-    const { decoded } = req;
+    const { decoded } = req.body;
     // return all requests if loggedin user is an admin
     if (decoded.role === 'admin') {
       res.status(200).json({ requests });
@@ -13,7 +13,7 @@ const requestsController = {
     }
   },
   getRequestById(req, res) {
-    const { decoded } = req;
+    const { decoded } = req.body;
     const requestId = Number(req.params.id);
     const request = requests.filter(element => element.id === requestId)[0];
     if (request) {
@@ -24,6 +24,47 @@ const requestsController = {
       }
     } else {
       res.status(404).json({ error: { message: 'request not found' } });
+    }
+  },
+  createRequest(req, res) {
+    const {
+      title,
+      description,
+      type,
+      decoded
+    } = req.body;
+    switch (false) {
+      case !!title: {
+        res.status(400).json({ error: { message: 'title is required' } });
+        break;
+      }
+      case !!description: {
+        res.status(400).json({ error: { message: 'description is required' } });
+        break;
+      }
+      case !!type: {
+        res.status(400).json({ error: { message: 'request type is required' } });
+        break;
+      }
+      default: {
+        // create id for new request by incrementing the id of the last request
+        const id = requests[requests.length - 1].id + 1;
+        const newRequest = {
+          id,
+          title,
+          description,
+          type,
+          resolved: false,
+          approved: false,
+          disapproved: false,
+          trashed: false,
+          feedback: '',
+          ownerId: decoded.id
+        };
+        // store the new request in memory
+        requests.push(newRequest);
+        res.status(201).json({ newRequest });
+      }
     }
   }
 };
