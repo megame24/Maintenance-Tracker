75809c04fdc77e5178a5606cea2e3ae0a22ac3b6
Innocent Ngene <ngeneinnocent@gmail.com>
2018-05-15 01:47:34 +0100

    [Chore #157581583] refactor middlewares and helpers

---
+++ server/helpers/JWToken.js
@@ -0,0 +1,18 @@
+import jwt from 'jsonwebtoken';
+
+const secret = 'hehehe';
+
+const JWToken = {
+  generateToken(userDetails) {
+    return jwt.sign(userDetails, secret, { expiresIn: 60 * 60 });
+  },
+  verifyToken(token) {
+    try {
+      return jwt.verify(token, secret);
+    } catch (err) {
+      return false;
+    }
+  }
+};
+
+export default JWToken;
