fc781d797ec6c15d8c9a89238b687db61cbca148
Innocent Ngene <ngeneinnocent@gmail.com>
2018-05-15 19:41:31 +0100

    [Feature #157606623] setup endpoint for user to delete his/her request

--- server/routes/requests.js
+++ server/routes/requests.js
@@ -7,5 +7,6 @@ router.post('/', requestsController.createRequest);
 router.get('/', requestsController.getRequests);
 router.get('/:id', requestsController.getRequestById);
 router.put('/:id', requestsController.updateRequest);
+router.delete('/:id', requestsController.deleteRequest);
 
 export default router;
