803efec8dd4747c2aae4b52ade1f068d07ecdfe6
Innocent Ngene <ngeneinnocent@gmail.com>
2018-05-17 12:00:34 +0100

    [Feature #157658118] test GET '/users/requests/<requestId>' route

--- server/test/testData.js
+++ server/test/testData.js
@@ -4,9 +4,16 @@ export default {
     username: 'john',
     password: 'wick'
   },
-  regularUser: {
+  regularUser1: {
     id: 4,
     username: 'uzumaki',
-    password: 'naruto'
+    password: 'naruto',
+    requestsId: [4, 6, 7, 8]
+  },
+  regularUser2: {
+    id: 3,
+    username: 'wizard12',
+    password: 'harry',
+    requestsId: [1, 3, 5]
   }
 };
