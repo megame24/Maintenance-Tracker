67a2c8b933f09464c3e8b1a573b3c74113da5d87
Innocent Ngene <ngeneinnocent@gmail.com>
2018-05-17 15:13:46 +0100

    [Feature #157660192] test PUT '/users/requests/<requestId>' route

--- server/test/testData.js
+++ server/test/testData.js
@@ -17,15 +17,34 @@ export default {
     requestsId: [1, 3, 5]
   },
   request1: {
+    id: 3,
+    title: 'Tire check',
+    description: 'My tire keeps making weird noises, its driving me crazy :(',
+    type: 'maintenance',
+    status: 'disapproved',
+    trashed: false,
+    feedback: '',
+    ownerId: 3
+  },
+  request2: {
+    id: 2,
+    title: 'Oil change',
+    description: 'The oil looks alright, but I have been using it for more than a 3 months. I want it changed.',
+    type: 'maintenance',
+    status: 'approved',
+    trashed: false,
+    feedback: 'Your request has been approved, and is being worked on',
+    ownerId: 2
+  },
+  request3: {
     id: 1,
     title: 'Broken side mirror',
-    description:
-      'The side mirror is in a very bad condition, the mirror is shattered and the mirror frame is badly damaged as well.',
+    description: 'The side mirror is in a very bad condition, the mirror is shattered and the mirror frame is badly damaged as well.',
     type: 'repair',
     status: 'pending',
     trashed: false,
-    feedback:
-      'Sorry, the requested service is not available at the moment. We are out of side mirror frames',
+    feedback: 'Sorry, the requested service is not available at the moment. We are out of side mirror frames',
     ownerId: 3
-  }
+  },
+  invalidId: 10000
 };
