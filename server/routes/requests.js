84457a35e4e7a00f667aad204b683ed3e8e1d0c9
Innocent Ngene <ngeneinnocent@gmail.com>
2018-05-18 20:05:19 +0100

    [Chore #157702265] refactor requestsController.js

--- server/routes/requests.js
+++ server/routes/requests.js
@@ -1,13 +1,44 @@
 import express from 'express';
 import requestsController from '../controllers/requestsController';
 import authMiddleware from '../middlewares/authMiddleware';
+import requestsMiddleware from '../middlewares/requestsMiddleware';
 
 const router = express.Router();
 
-router.post('/', authMiddleware.verifyUser, requestsController.createRequest);
 router.get('/', authMiddleware.verifyUser, requestsController.getRequests);
-router.get('/:id', authMiddleware.verifyUser, requestsController.getRequestById);
-router.put('/:id', authMiddleware.verifyUser, requestsController.updateRequest);
-router.delete('/:id', authMiddleware.verifyUser, requestsController.deleteRequest);
+router.post(
+  '/',
+  [authMiddleware.verifyUser, requestsMiddleware.isAUser],
+  requestsController.createRequest
+);
+router.get(
+  '/:id',
+  [
+    authMiddleware.verifyUser,
+    requestsMiddleware.findRequest,
+    requestsMiddleware.adminOrOwner
+  ],
+  requestsController.getRequestById
+);
+router.put(
+  '/:id',
+  [
+    authMiddleware.verifyUser,
+    requestsMiddleware.findRequest,
+    requestsMiddleware.adminOrOwner,
+    requestsMiddleware.beforeUpdate
+  ],
+  requestsController.updateRequest
+);
+router.delete(
+  '/:id',
+  [
+    authMiddleware.verifyUser,
+    requestsMiddleware.findRequest,
+    requestsMiddleware.adminOrOwner,
+    requestsMiddleware.freeToDelete
+  ],
+  requestsController.deleteRequest
+);
 
 export default router;
