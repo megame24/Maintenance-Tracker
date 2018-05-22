c68b8cdd2c16982b310d058411ecd7f7761aa9b3
Innocent Ngene <ngeneinnocent@gmail.com>
2018-05-22 09:12:35 +0100

    implement first feedback

--- UI/js/script.js
+++ UI/js/script.js
@@ -7,37 +7,34 @@ var mobileMenuToggle = document.querySelector(".toggle-menu");
 var navHeader = document.querySelector(".nav-header");
 var rightMenu = document.querySelector(".right-menu");
 mobileMenuToggle.onclick = function() {
-    if (!rightMenu.classList.contains("not-mobile")) {
-      rightMenu.classList.add("not-mobile");
-      navHeader.classList.add("nav-header-shadow");
-    } else {
-      rightMenu.classList.remove("not-mobile");
-      navHeader.classList.remove("nav-header-shadow");
-    }
+    rightMenu.classList.toggle("not-mobile");
+    navHeader.classList.toggle("nav-header-shadow");
 }
 
 var navItemAdmin = document.querySelector(".admin .nav-item");
 var dropDownMenuAdmin = document.querySelector(".admin .drop-down-menu");
-navItemAdmin.onclick = function() {
-    toggleHide(dropDownMenuAdmin);
+if (navItemAdmin) {
+  navItemAdmin.onclick = function() {
+      toggleHide(dropDownMenuAdmin);
+  }
 }
 
 var navItemRequest = document.querySelector(".request .nav-item");
 var dropDownMenuRequest = document.querySelector(".request .drop-down-menu");
-navItemRequest.onclick = function() {
-    toggleHide(dropDownMenuRequest);
+if (navItemRequest) {
+  navItemRequest.onclick = function() {
+      toggleHide(dropDownMenuRequest);
+  }
 }
 
 var navItemUser = document.querySelector(".user .nav-item");
 var dropDownMenuUser = document.querySelector(".user .drop-down-menu");
-navItemUser.onclick = function() {
-    toggleHide(dropDownMenuUser);
+if (navItemRequest) {
+  navItemUser.onclick = function() {
+      toggleHide(dropDownMenuUser);
+  }
 }
 
 function toggleHide(element) {
-  if (!element.classList.contains("hide")) {
-    element.classList.add("hide");
-  } else {
-    element.classList.remove("hide");
-  }
+  element.classList.toggle('hide');
 };
