c0588e285954800b6b53bf9337f29ef5cf108852
Innocent Ngene <ngeneinnocent@gmail.com>
2018-05-24 02:11:27 +0100

    [Feature #157793037] login user with persistance

--- server/db/seeders/index.js
+++ server/db/seeders/index.js
@@ -7,34 +7,72 @@ import requests from './seed-requests';
 
 db.query('DELETE FROM users', [])
   .then(() => db.query('TRUNCATE TABLE users RESTART IDENTITY', []))
-  .then(() => {
-    users.forEach((elem) => {
-      db.query(
-        'INSERT INTO users (fullname, username, email, password, role) VALUES ($1, $2, $3, $4, $5)',
-        Object.values(elem).map(el => el)
-      )
-        .catch((err) => {
-          console.log(err);
-        });
-    });
-  })
+  .then(() => 
+    db.query(
+      'INSERT INTO users (fullname, username, email, password, role) VALUES ($1, $2, $3, $4, $5)',
+      Object.values(users[0]).map(el => el)
+    ))
+  .then(() => 
+    db.query(
+      'INSERT INTO users (fullname, username, email, password, role) VALUES ($1, $2, $3, $4, $5)',
+      Object.values(users[1]).map(el => el)
+    ))
+  .then(() => 
+    db.query(
+      'INSERT INTO users (fullname, username, email, password, role) VALUES ($1, $2, $3, $4, $5)',
+      Object.values(users[2]).map(el => el)
+    ))
+  .then(() => 
+    db.query(
+      'INSERT INTO users (fullname, username, email, password, role) VALUES ($1, $2, $3, $4, $5)',
+      Object.values(users[3]).map(el => el)
+    ))
   .catch((err) => {
     console.log(err);
   });
 
 db.query('DELETE FROM requests', [])
   .then(() => db.query('TRUNCATE TABLE requests RESTART IDENTITY', []))
-  .then(() => {
-    requests.forEach((elem) => {
-      db.query(
-        'INSERT INTO requests (title, description, type, status, trashed, feedback, owner, date, ownerId) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)',
-        Object.values(elem).map(el => el)
-      )
-        .catch((err) => {
-          console.log(err);
-        });
-    });
-  })
+  .then(() => 
+    db.query(
+      'INSERT INTO requests (title, description, type, status, trashed, feedback, owner, date, ownerId) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)',
+      Object.values(requests[0]).map(el => el)
+    ))
+  .then(() => 
+    db.query(
+      'INSERT INTO requests (title, description, type, status, trashed, feedback, owner, date, ownerId) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)',
+      Object.values(requests[1]).map(el => el)
+    ))
+  .then(() => 
+    db.query(
+      'INSERT INTO requests (title, description, type, status, trashed, feedback, owner, date, ownerId) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)',
+      Object.values(requests[2]).map(el => el)
+    ))
+  .then(() => 
+    db.query(
+      'INSERT INTO requests (title, description, type, status, trashed, feedback, owner, date, ownerId) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)',
+      Object.values(requests[3]).map(el => el)
+    ))
+  .then(() => 
+    db.query(
+      'INSERT INTO requests (title, description, type, status, trashed, feedback, owner, date, ownerId) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)',
+      Object.values(requests[4]).map(el => el)
+    ))
+  .then(() => 
+    db.query(
+      'INSERT INTO requests (title, description, type, status, trashed, feedback, owner, date, ownerId) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)',
+      Object.values(requests[5]).map(el => el)
+    ))
+  .then(() => 
+    db.query(
+      'INSERT INTO requests (title, description, type, status, trashed, feedback, owner, date, ownerId) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)',
+      Object.values(requests[6]).map(el => el)
+    ))
+  .then(() => 
+    db.query(
+      'INSERT INTO requests (title, description, type, status, trashed, feedback, owner, date, ownerId) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)',
+      Object.values(requests[7]).map(el => el)
+    ))
   .catch((err) => {
     console.log(err);
   });
--- server/db/seeders/seed-users.js
+++ server/db/seeders/seed-users.js
@@ -1,30 +1,37 @@
+
+import bcrypt from 'bcrypt';
+
+require('dotenv').config();
+
+const salt = Number(process.env.SALT);
+
 export default [
   {
     fullname: 'John Wick',
     username: 'john',
     email: 'johnwick@gmail.com',
-    password: 'wick',
+    password: bcrypt.hashSync('wick', salt),
     role: 'admin'
   },
   {
     fullname: 'Monkey D. Luffy',
     username: 'strawhat',
     email: 'monkeydluffy@gmail.com',
-    password: 'monkey',
+    password: bcrypt.hashSync('monkey', salt),
     role: 'user'
   },
   {
     fullname: 'Harry Potter',
     username: 'wizard12',
     email: 'harrypotter@gmail.com',
-    password: 'harry',
+    password: bcrypt.hashSync('harry', salt),
     role: 'user'
   },
   {
     fullname: 'Uzumaki Naruto',
     username: 'uzumaki',
     email: 'uzumakinaruto@gmail.com',
-    password: 'naruto',
+    password: bcrypt.hashSync('naruto', salt),
     role: 'user'
   }
 ];
