db0dba9a4c70e189f8435fc51590a2b69358af1f
Innocent Ngene <ngeneinnocent@gmail.com>
2018-05-30 14:42:20 +0100

    chore(refactor client): refactor makeRequest.js

--- client/js/handleError.js
+++ client/js/handleError.js
@@ -5,3 +5,11 @@ const handleRedirectError = (errMessage, subUrl) => {
   queryString = window.btoa(queryString);
   window.location = `${baseUrl}/${subUrl}?${queryString}`;
 };
+
+const displayError = (message) => {
+  submitBtn.disabled = false;
+  submitBtn.classList.remove('disabled');
+  submitBtn.value = 'Make Request';
+  errorMessage.innerText = message;
+  errorMessage.classList.remove('hide');
+};
--- client/js/makeRequest.js
+++ client/js/makeRequest.js
@@ -1,66 +1,59 @@
+/* eslint-disable no-unused-vars */
+let titleField,
+  descriptionField,
+  typeField,
+  submitBtn,
+  errorMessage;
+
+const url = `${baseUrl}/api/v1/users/requests`;
+const headers = new Headers();
+headers.append('Content-Type', 'application/json');
+headers.append('authorization', token);
+
+const makeRequest = (request) => {
+  fetch(request)
+    .then(res => res.json())
+    .then((result) => {
+      if (result.error) {
+        displayError(result.error.message);
+        return;
+      }
+      const message = {
+        success: true,
+        message: result.success.message
+      };
+      let queryString = JSON.stringify(message);
+      queryString = window.btoa(queryString);
+      window.location = `${baseUrl}/view-requests.html?${queryString}`;
+    });
+};
+
+const makeRequestController = (event) => {
+  event.preventDefault();
+  submitBtn.disabled = true;
+  submitBtn.classList.add('disabled');
+  submitBtn.value = 'Making Request...';
+  let formData = {
+    title: titleField.value,
+    description: descriptionField.value,
+    type: typeField.value
+  };
+  formData = JSON.stringify(formData);
+  const request = new Request(url, { method: 'POST', headers, body: formData });
+  makeRequest(request);
+};
 
 const init = () => {
   const makeRequestForm = document.getElementById('make-request-form');
-  const titleField = document.getElementById('title');
-  const descriptionField = document.getElementById('description');
-  const typeField = document.querySelector('input[name=type]:checked');
-  const submitBtn = document.getElementById('submit-btn');
-  const errorMessage = document.getElementsByClassName('error-message')[0];
-  const displayUsername = document.getElementById('display-username');
-
-  const baseUrl = window.location.origin;
-  const url = `${baseUrl}/api/v1/users/requests`;
-  // perseJwt function from stackoverflow >> https://stackoverflow.com/a/38552302
-  const parseJwt = (jwToken) => {
-    const base64Url = jwToken.split('.')[1];
-    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    return JSON.parse(window.atob(base64));
-  };
-  const token = window.localStorage.getItem('token');
-  const userDetails = parseJwt(token);
+  titleField = document.getElementById('title');
+  descriptionField = document.getElementById('description');
+  typeField = document.querySelector('input[name=type]:checked');
+  submitBtn = document.getElementById('submit-btn');
+  errorMessage = document.getElementsByClassName('error-message')[0];
+  displayUsername = document.getElementById('display-username');
+  userDetails = parseJwt(token);
   displayUsername.append(userDetails.username);
-  makeRequestForm.onsubmit = (event) => {
-    event.preventDefault();
-
-    submitBtn.disabled = true;
-    submitBtn.classList.add('disabled');
-    submitBtn.value = 'Making Request...';
-
-    let formData = {
-      title: titleField.value,
-      description: descriptionField.value,
-      type: typeField.value
-    };
-    formData = JSON.stringify(formData);
-
-    const headers = new Headers();
-    headers.append('Content-Type', 'application/json');
-    headers.append('authorization', token);
-    const request = new Request(url, {
-      method: 'POST',
-      headers,
-      body: formData
-    });
-    fetch(request)
-      .then(res => res.json())
-      .then((result) => {
-        if (result.error) {
-          submitBtn.disabled = false;
-          submitBtn.classList.remove('disabled');
-          submitBtn.value = 'Make Request';
-          errorMessage.innerText = result.error.message;
-          errorMessage.classList.remove('hide');
-          return;
-        }
-        const message = {
-          success: true,
-          message: result.success.message
-        };
-        let queryString = JSON.stringify(message);
-        queryString = window.btoa(queryString);
-        window.location = `${baseUrl}/view-requests.html?${queryString}`;
-      });
-  };
+  makeRequestForm.addEventListener('submit', makeRequestController);
 };
 
 window.onload = init();
