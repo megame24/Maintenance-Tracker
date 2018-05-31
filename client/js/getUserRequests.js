7d677a33d482faa80dc79044d17297462fed7069
Innocent Ngene <ngeneinnocent@gmail.com>
2018-05-31 12:03:17 +0100

    chore(html clean): clean up html files

--- client/js/getUserRequests.js
+++ client/js/getUserRequests.js
@@ -1,5 +1,6 @@
 let errorMessage,
   successMessage,
+  noRequestDiv,
   tableBody;
 const url = `${baseUrl}/api/v1/users/requests`;
 
@@ -23,6 +24,10 @@ const statusDependents = (status) => {
 };
 
 const populateTableWithRequests = (result) => {
+  if (result.length === 0) {
+    noRequestDiv.classList.add('no-requests');
+    noRequestDiv.innerHTML = '<i class="ion-plus-circled icon"></i><br /><br /><p>You have no requests at the moment, <a href="/make-request.html" class="btn btn-primary btn-small">click here</a> to make a request</p>';
+  }
   result.forEach((elem) => {
     const row = createNode('tr');
     row.innerHTML = `<td class="dark-blue table-link"><a href="/user-request-details.html?${elem.id}">${elem.title}</a></td><td>${elem.type}</td><td class=${statusDependents(elem.status).backgroundColor}>${statusDependents(elem.status).message}</td>`;
@@ -35,8 +40,7 @@ headers.append('authorization', token);
 const request = new Request(url, { method: 'GET', headers });
 
 const getUserRequests = () => {
-  fetch(request)
-    .then(res => res.json())
+  fetch(request).then(res => res.json())
     .then((result) => {
       if (result.error) {
         handleRedirectError(result.error.message, 'login.html');
@@ -48,6 +52,7 @@ const getUserRequests = () => {
 
 const init = () => {
   tableBody = document.getElementById('table-body');
+  noRequestDiv = document.getElementById('no-requests');
   const displayUsername = document.getElementById('display-username');
   errorMessage = document.getElementsByClassName('error-message')[0];
   successMessage = document.getElementsByClassName('success-message')[0];
